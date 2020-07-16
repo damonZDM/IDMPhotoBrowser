ce32b5fce0e9e239b97eaf96b29ed45c069fb2ce
Damon <zhaodm1002@163.com>
2020-07-16 10:55:39 +0800

    init framewrok

---
+++ IDMPhotoBrowser/IDMPhotoBrowser/IDMPhotoBrowser.h
@@ -0,0 +1,19 @@
+//
+//  IDMPhotoBrowser.h
+//  IDMPhotoBrowser
+//
+//  Created by damon on 2020/7/16.
+//  Copyright © 2020 damon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDMPhotoBrowser.
+FOUNDATION_EXPORT double IDMPhotoBrowserVersionNumber;
+
+//! Project version string for IDMPhotoBrowser.
+FOUNDATION_EXPORT const unsigned char IDMPhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDMPhotoBrowser/PublicHeader.h>
+
+
